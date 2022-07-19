ef63b8352171a1fc87d2cc696a678949b900a6b7
John Eleanor <mrs.ch64@gmail.com>
2022-07-19 22:31:38 +0700

    [+] Add System register.php 7/19/2022

---
+++ System/register.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'config.php';
+
+    if(isset($_POST['btnInsert'])){ 
+
+        if ($_POST['confirm-password'] != $_POST['password']){
+            echo "<script type = text/javascript>alert('รหัสผ่านไม่ตรงกัน');</script>";
+            header('location:../index.php');
+        }else{
+
+            $sql = "INSERT INTO tb_member (`Name`, `LastName`, `EMail`, `Mobile`, `Address`, `Province`, `District`, `SubDistrict`, `ZipCode`, `Password`, `Status`, `FarmersGroup`) value ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['email']."', '".$_POST['phone']."','".$_POST['address']."','".$_POST['province']."','".$_POST['district']."','".$_POST['subdistrict']."', '".$_POST['zipcode']."', '".$_POST['password']."', '".$_POST['type']."', '".$_POST['Group']."')";
+            echo($sql);
+            $query = mysqli_query($conn, $sql);
+            if ($query){
+                echo "<br>เพิ่มข้อมูลสำเร็จ";
+                echo "<script type = text/javascript>alert('เพิ่มข้อมูลสำเร็จ');</script>";
+                header('location: ../login.php');
+            }else{
+                echo "<br>เพิ่มข้อมูลไม่สำเร็จ";
+                header('location:../index.php');
+            }
+
+        }
+    }
+?>
