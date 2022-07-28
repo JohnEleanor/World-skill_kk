114cb33a48e10af5ef9aea41e9c65e4e4a925818
John Eleanor <mrs.ch64@gmail.com>
2022-07-29 00:18:03 +0700

    else if [Delete]

--- System/register.php
+++ System/register.php
@@ -6,10 +6,7 @@ require_once 'config.php';
 
         if ($_POST['confirm-password'] != $_POST['password']){
             echo "<script type = text/javascript>alert('รหัสผ่านไม่ตรงกัน');</script>";
-            header('Refresh:0 ../index.php');
-        }elseif ($_POST['type'] == "" || $_POST['group'] == "" || $_POST['firstname'] == ""|| $_POST['lastname'] == ""|| $_POST['email'] == ""|| $_POST['phone'] == ""|| $_POST['address'] == ""|| $_POST['province'] == ""|| $_POST['district'] == ""|| $_POST['subdistrict'] == ""|| $_POST['zipcode'] == ""|| $_POST['password'] == ""){
-            echo "<script type = text/javascript>alert('กรุณากรอกข้อมูลให้สำเร็จ');</script>";
-            header('Refresh:0 ../index.php');
+            header('Location ../register.php');
         }else{
 
             $sql = "INSERT INTO tb_member (`Name`, `LastName`, `EMail`, `Mobile`, `Address`, `Province`, `District`, `SubDistrict`, `ZipCode`, `Password`, `Status`, `FarmersGroup`) value ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['email']."', '".$_POST['phone']."','".$_POST['address']."','".$_POST['province']."','".$_POST['district']."','".$_POST['subdistrict']."', '".$_POST['zipcode']."', '".$_POST['password']."', '".$_POST['type']."', '".$_POST['Group']."')";
